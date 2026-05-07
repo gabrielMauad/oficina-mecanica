f6416eee998e6cb1e53a9b8e68c8ed3e707e1ec7
gmauad <gabrielbms711@gmail.com>
2026-05-07 00:18:17 -0300

    feat: T14 - Peças e Insumos Contracts

---
+++ src/Modules/PecasInsumos/PecasInsumos.Contracts/Queries/IPecaInsumoQuery.cs
@@ -0,0 +1,8 @@
+﻿using PecasInsumos.Contracts.Dtos;
+
+namespace PecasInsumos.Contracts.Queries;
+
+public interface IPecaInsumoQuery
+{
+    Task<PecaInsumoResumoDto?> ObterPorId(Guid id, CancellationToken ct = default);
+}
---
+++ src/Modules/PecasInsumos/PecasInsumos.Contracts/Queries/IPecasInsumosDisponibilidadeQuery.cs
@@ -0,0 +1,9 @@
+﻿using PecasInsumos.Contracts.Dtos;
+
+namespace PecasInsumos.Contracts.Queries;
+
+public interface IPecasInsumosDisponibilidadeQuery
+{
+    Task<DisponibilidadeDto> VerificarDisponibilidade(Guid pecaId, int quantidade, CancellationToken ct = default);
+}
+
